a8b6b4a80fab081f2dbb65b69847a09e47834bc4
devi <engineerdevi@gmail.com>
2015-10-22 09:59:43 -0500

    Programming Assignment #2 On Oct 22, 2015
    Edited locally and did commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  
+  invMatrix <- NULL
+  
+  setMatrix <- function(y){
+    x <<- y
+    invMatrix <<- NULL
+  }
+  ##Output Function
+  getMatrix <- function() {
+    x
+  }
+  #setting inverse Matrix
+  setInverseMatrix <- function(inv){
+    invMatrix <<- inv
+  }
+  ## Get Inverse Matrix
+  getInverseMatrix <- function() {
+     invMatrix
+  }
+ 
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix, setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix )
+  
 }
 
 
@@ -12,4 +35,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m1 <- x$getInverseMatrix()
+ 
+  if(!is.null(m1) ) {
+    message("Getting Matrix from the Cache...")
+    return(m1)
+  }
+  m2 <- x$getMatrix()
+  m1 <- solve(m2)
+  x$setInverseMatrix(m1)
+  return(m1)
 }
